9bf016cc1e89fd013cdb4c0402bde6455885fec3
Shvike <shvike@mail.ru>
2020-06-14 12:33:56 +0300

    add video content

--- showvideo/urls.py
+++ showvideo/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     re_path('123/', views.hello),
-    re_path('456/', views.world),
+    re_path('456/', views.world, name="main_page"),
+    re_path("comment/(?P<id>[-\d]+)/", views.accept_comment),
+    re_path("only_video/(?P<id>[-\d]+)/", views.only_video),
+    re_path("add_like/(?P<id>[-\d]+)/", views.add_like),
 ]
--- showvideo/views.py
+++ showvideo/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Video
+from .models import Video, Comment
 
 
 def hello(request):
@@ -12,3 +12,32 @@ def world(request):
     response = {"name":"Alex", "d":34, "arr":"12345"}
     response["content"] = Video.objects.all()
     return render(request, "index.html", response)
+
+def accept_comment(request, id):
+
+    Comment.objects.create(text=request.POST["commentary"], comment_video_id=id)
+    print(request.POST)
+    # 1
+    # Comment.objects.create(text=request.GET["commentary"], comment_video_id=id)
+
+    # 2
+    # vid = Video.objects.get(id=id)
+    # Comment.objects.create(text="hi text", comment_video=vid)
+
+    # 3
+    # c = Comment(text=request.GET["commentary"], comment_video_id=id)
+    # c.save()
+
+    # print(id)
+    # print(request.GET["commentary"])
+    return redirect("main_page")
+
+def only_video(request, id):
+    response = {"video":Video.objects.get(id=id)}
+    return render(request, "only_video.html", response)
+
+def add_like(request, id):
+    v = Video.objects.get(id=id)
+    v.likes += 2
+    v.save()
+    return redirect("main_page")
